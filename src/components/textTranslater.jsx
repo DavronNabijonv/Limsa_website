1e8824e22c33d00d9ab8cb89e739767cdc786ab0
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-10 03:37:36 +0500

    text_translation

---
+++ src/components/textTranslater.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+export default function TextTranslater({txt}) {
+    const {t} = useTranslation();
+  return (
+    <div>{t(txt)}</div>
+  )
+}
