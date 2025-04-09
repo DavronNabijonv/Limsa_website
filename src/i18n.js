267552be89221d9884ad1f4a1af0f1bce42afab2
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-09 23:27:58 +0500

    mainly-files

---
+++ src/i18n.js
@@ -0,0 +1,4 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Bcakend from 'i18next-http-backend';
+import laungageDetector from 'i18next-browser-languagedetector'
