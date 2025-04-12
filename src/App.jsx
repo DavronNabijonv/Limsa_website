20805d68dd6d00d2e5ec92160ff99c1648802e71
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-04-13 03:05:00 +0500

    hamePage-parts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./sections/nav_contactFooter/navbar";
 import HOMEPAGE from "./sections/HOMEPAGE";
+import Our_works from "./sections/our_works";
 
 function App() {
   const routes = [
     {
       id: 1,
       path: "/",
+      element: <HOMEPAGE />,
+    },
+    {
+      id: 2,
+      path: "/xizmat",
+      element: <HOMEPAGE />,
+    },
+    {
+      id: 3,
+      path: "/ishlar",
+      element: <Our_works />,
+    },
+    {
+      id: 4,
+      path: "/narxlar",
       element: <HOMEPAGE />,
     },
   ];
